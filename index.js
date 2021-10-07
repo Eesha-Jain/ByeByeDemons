68ee8aaf9e7e6437a070d0c58da97d7f6a764483
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2021-10-07 04:37:20 +0000

    Experimenting with word processing libraries

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile('/index.html', {root:'.'});
 });
 
-app.post('/getdemons', function (req, res) { 
-  res.status(200).send(JSON.stringify({error: false, demons: true, demonWords: ["i"]}));
-})
-
 const port = process.env.PORT || 8080
 app.listen(port, () => {
   console.log("Server ran!") 
