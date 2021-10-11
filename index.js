8f53e0ff5df59c762279f4f23e8cc661f500a2eb
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2021-10-11 03:41:09 +0000

    Got putting file on server and reading it to work

--- index.js
+++ index.js
@@ -1,59 +1,47 @@
 let express = require('express');
 let app = express();
-var multer = require('multer');
 const spawn = require('child_process').spawn;
+var mammoth = require("mammoth");
+var multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: function (request, file, callback) {
+        console.log(file);
+        callback(null, './uploads/');
+    },
+    filename: function (request, file, callback) {
+        console.log(file);
+        callback(null, "uploaded")
+    }
+});
+
+var upload = multer({ storage: storage });
 
 app.use('/css',express.static(__dirname +'/css'));
 app.use(express.json());
-
-//Storage and upload are modified from https://www.geeksforgeeks.org/file-uploading-in-node-js/
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads")
-  },
-  filename: function (req, file, cb) {
-    cb(null, "uploaded.docx")
-  }
-})
-
-var upload = multer({ 
-  storage: storage 
-}).single("doc");
 
 app.get('/', function (req, res) {
   res.sendFile('/index.html', {root:'.'});
 });
 
-app.post('/fileupload', async (req, res) => {
-  //Convert into word document in server
-  await upload(req, res, function(err) {})
+app.post('/fileupload', upload.single('avatar'), async function (req, res, next) {
+  var demons;
+  var text;
 
   //Get demon words
   const pythonProcess = await spawn('python', ['./python/demons.py']);
-  var demons;
 
   await pythonProcess.stdout.on('data', (data) => {
-    console.log("1: " + data.toString());
     demons = data.toString();
   });
 
-  console.log("2: " + demons);
-
   //Read uploaded doc
-  const pyProcess = await spawn('python', ['./python/scripts.py']);
-  var docContents;
-
-  console.log("3: " + docContents);
-
-  await pyProcess.stdout.on('data', (data) => {
-    console.log("4: " + data.toString());
-    docContents = data.toString();
-  });
-
-  console.log("5 output");
+  await mammoth.extractRawText({path: req.file.path}).then(function(result) {
+    text = result.value;
+  }).done();
 
   //Return
-  //return res.status(200).send(JSON.stringify({demon: demons, docContent: docContents}))
+  return res.status(200).send(JSON.stringify({demon: demons, docContent: text}))
 })
 
 const port = process.env.PORT || 8080
